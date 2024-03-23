88e8458f2febddc0bcd8a03c04518419198d880d
rishu <rishabhkaparwan8@gmail.com>
2024-03-23 14:35:28 +0530

    23rd mar, 2024

---
+++ Javadocdemo.java
@@ -0,0 +1,13 @@
+/**
+ * @author RISHU
+ *
+ */
+package javadocdemo;
+
+public class Javadocdemo {
+
+    public static void main(String[] args) {
+        System.out.println("Book.java is connected with this class");
+    }
+    
+}
