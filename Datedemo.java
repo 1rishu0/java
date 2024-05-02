1ba77797db9e3be84aacc35c371aeb06ddf9a4cf
rishu <rishabhkaparwan8@gmail.com>
2024-05-02 10:46:54 +0530

    02nd may , 2024

--- Datedemo.java
+++ Datedemo.java
@@ -5,6 +5,8 @@
 package datedemo;
 
 import java.util.*;
+import java.time.*;
+import java.time.temporal.*;
 
 /**
  *
@@ -20,24 +22,60 @@ public class Datedemo
     {
         // TODO code application logic here
         
-//        Date d=new Date("4/8/2003");
+        //PART-1
+        
+////        Date d=new Date("4/8/2003");
+//        Date d=new Date();
+//        
+//        GregorianCalendar gc=new GregorianCalendar();
+//        
+//        TimeZone tz=gc.getTimeZone();
+//        
+//        System.out.println(tz.getID());
+//        gc.setTimeZone(TimeZone.getTimeZone("America/Los_Angeles"));
+//        
+//        System.out.println(gc.isLeapYear(2024));
+//        System.out.println(Calendar.DATE);
+//
+//        
+//        System.out.println(System.currentTimeMillis());
+//        System.out.println(d);
+//        System.out.println(d.getDay());
+//        System.out.println(d.getYear()+1900);
+        
+        
+        //PART-2
+//Joda API have separate class for local date , local time and local date time .
+//These class contain time in seconds and nano seconds.
+//The year which is 1970 the start of the year is called the epoch day.
+        
         Date d=new Date();
-        
-        GregorianCalendar gc=new GregorianCalendar();
-        
-        TimeZone tz=gc.getTimeZone();
-        
-        System.out.println(tz.getID());
-        gc.setTimeZone(TimeZone.getTimeZone("America/Los_Angeles"));
-        
-        System.out.println(gc.isLeapYear(2024));
-        System.out.println(Calendar.DATE);
-
-        
-        System.out.println(System.currentTimeMillis());
+        d.setHours(21);
         System.out.println(d);
-        System.out.println(d.getDay());
-        System.out.println(d.getYear()+1900);
+        
+//        LocalDate ld=LocalDate.now();
+//        LocalDate ld=LocalDate.now(ZoneId.of("Asia/Kolkata"));
+//        LocalDate ld=LocalDate.ofEpochDay(1);//1970
+        LocalDate ld=LocalDate.parse("2024-08-04");
+        
+        LocalDate ld1=ld.plusMonths(4);
+        ld1=ld1.plusDays(4);
+        
+        System.out.println(ld1);
+        
+        
+        LocalTime lt=LocalTime.now();
+        
+        LocalTime lt1=lt.plusHours(4);
+        
+        System.out.println(lt);        
+        System.out.println(lt1);
+        
+        
+        LocalDateTime ldt=LocalDateTime.now();
+        
+        System.out.println(ldt);
+        
     }
     
 }
