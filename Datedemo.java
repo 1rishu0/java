3ff040fb4cf0dbcb32be41e76e566f5e11992f00
rishu <rishabhkaparwan8@gmail.com>
2024-05-03 10:52:12 +0530

    03rd may , 2024

--- Datedemo.java
+++ Datedemo.java
@@ -7,6 +7,7 @@ package datedemo;
 import java.util.*;
 import java.time.*;
 import java.time.temporal.*;
+import java.time.format.*;
 
 /**
  *
@@ -49,33 +50,62 @@ public class Datedemo
 //These class contain time in seconds and nano seconds.
 //The year which is 1970 the start of the year is called the epoch day.
         
-        Date d=new Date();
-        d.setHours(21);
-        System.out.println(d);
+//        Date d=new Date();
+//        d.setHours(21);
+//        System.out.println(d);
+//        
+////        LocalDate ld=LocalDate.now();
+////        LocalDate ld=LocalDate.now(ZoneId.of("Asia/Kolkata"));
+////        LocalDate ld=LocalDate.ofEpochDay(1);//1970
+//        LocalDate ld=LocalDate.parse("2024-08-04");
+//        
+//        LocalDate ld1=ld.plusMonths(4);
+//        ld1=ld1.plusDays(4);
+//        
+//        System.out.println(ld1);
+//        
+//        
+//        LocalTime lt=LocalTime.now();
+//        
+//        LocalTime lt1=lt.plusHours(4);
+//        
+//        System.out.println(lt);        
+//        System.out.println(lt1);
+//        
+//        
+//        LocalDateTime ldt=LocalDateTime.now();
+//        
+//        System.out.println(ldt);
+//        
         
-//        LocalDate ld=LocalDate.now();
-//        LocalDate ld=LocalDate.now(ZoneId.of("Asia/Kolkata"));
-//        LocalDate ld=LocalDate.ofEpochDay(1);//1970
-        LocalDate ld=LocalDate.parse("2024-08-04");
         
-        LocalDate ld1=ld.plusMonths(4);
-        ld1=ld1.plusDays(4);
+        //PART-3
         
-        System.out.println(ld1);
+//        ZonedDateTime zdt=ZonedDateTime.now();
+//        ZonedDateTime zdt1=ZonedDateTime.now(ZoneId.of("America/Los_Angeles"));
+//        OffsetDateTime odt=OffsetDateTime.now();
+//        MonthDay md=MonthDay.now();
+//        Period p=Period.of(2, 2, 10);
+//        Instant i=Instant.now();
+//        
+//        System.out.println(zdt);
+//        System.out.println(zdt1);
+//        System.out.println(odt);
+//        System.out.println(p.addTo(LocalDate.now()));
+//        System.out.println(i);//IT SHOWS GREENWICH MEAN TIME
         
         
-        LocalTime lt=LocalTime.now();
-        
-        LocalTime lt1=lt.plusHours(4);
-        
-        System.out.println(lt);        
-        System.out.println(lt1);
-        
+        //PART-4
         
         LocalDateTime ldt=LocalDateTime.now();
+//        ZonedDateTime ldt=ZonedDateTime.now();
+
+//        DateTimeFormatter df=DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter df=DateTimeFormatter.ofPattern("dd/MM/yyyy hh:mm:ss");
+//        DateTimeFormatter df=DateTimeFormatter.ofPattern("dd/MM/yyyy hh:mm:ss z Z");
         
-        System.out.println(ldt);
-        
+        System.out.println(df.format(ldt));
+        System.out.println(ldt.get(ChronoField.DAY_OF_MONTH));
     }
     
 }
