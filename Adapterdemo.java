b8d0e809d0a234460927a46b3a1ad81eef422bde
Rishu <rishabhkaparwan8@gmail.com>
2024-07-21 09:26:00 +0530

    21st july, 2024

--- Adapterdemo.java
+++ Adapterdemo.java
@@ -3,11 +3,53 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package adapterdemo;
+import java.awt.*;
+import java.awt.event.*;
 
 /**
  *
- * @author RISHU
+ * @author RISHAB
  */
+////    create another calss to handle the window adapter
+//    class adapter extends WindowAdapter
+//    {
+//        public void windowClosing(WindowEvent e)
+//        {
+//            System.exit(0);
+//        }
+//    }
+//create new class to handle the frame
+class myframe extends Frame
+{
+//    create a constructor for the automation of the frame
+    myframe()
+    {
+//        call the super class and write Adapter demo
+        super("Adapter Demo");
+        
+////        create the object for adapter class
+//        adapter a=new adapter();
+
+////        add the window adapter to the frame
+//        addWindowListener(a);
+
+//       another way to add the window adapter to the frame
+        addWindowListener(new WindowAdapter(){
+            public void windowClosing(WindowEvent we)
+            {
+                System.exit(0);
+            }
+        });
+    }
+////    create another calss to handle the window adapter
+//    class adapter extends WindowAdapter
+//    {
+//        public void windowClosing(WindowEvent e)
+//        {
+//            System.exit(0);
+//        }
+//    }
+}
 public class Adapterdemo {
 
     /**
@@ -15,6 +57,12 @@ public class Adapterdemo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+//        create the object for myframe
+        myframe mf=new myframe();
+//        set the size of the myframe
+        mf.setSize(500,500);
+//        set the visiblity of the frame to true
+        mf.setVisible(true);
     }
     
 }
