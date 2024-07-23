69894a84d317934adc2bed69d62828c152f5d165
Rishu <rishabhkaparwan8@gmail.com>
2024-07-23 09:32:11 +0530

    23rd july, 2024

--- Buttondemo.java
+++ Buttondemo.java
@@ -4,10 +4,40 @@
  */
 package buttondemo;
 
+import java.awt.*;
+import java.awt.event.*;
+
 /**
  *
- * @author RISHU
+ * @author RISHAB
  */
+
+class myframe extends Frame implements ActionListener
+{
+    int count =0;
+    Label l;
+    Button b;
+    
+    public myframe()
+    {
+        super("Button Demo");
+        
+        l=new Label("   "+count);
+        b=new Button("Click");
+        b.addActionListener(this);
+        
+        setLayout(new FlowLayout());
+        
+        add(l);
+        add(b);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        count++;
+        l.setText("   "+count);
+    }
+}
+
 public class Buttondemo {
 
     /**
@@ -15,6 +45,12 @@ public class Buttondemo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        myframe f=new myframe();
+        
+        f.setSize(400,400);
+        f.setVisible(true);
+        
     }
     
 }
