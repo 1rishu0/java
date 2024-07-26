7f793939127adab71c0bc07342922a56b069f108
Rishu <rishabhkaparwan8@gmail.com>
2024-07-26 08:32:09 +0530

    26th july,2024

--- Datagramclient.java
+++ Datagramclient.java
@@ -4,17 +4,53 @@
  */
 package datagramclient;
 
+import java.net.*;
+
 /**
  *
- * @author RISHU
+ * @author RISHAB
  */
 public class Datagramclient {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws Exception{
+        DatagramSocket ds=new DatagramSocket(2000);
+        String msg="Hello World";
+        DatagramPacket dp=new DatagramPacket(msg.getBytes(),msg.length(),InetAddress.getByName("localhost"),2001);
+        ds.send(dp);
+        
+        byte[] b=new byte[1024];
+        dp=new DatagramPacket(b,1024);
+        ds.receive(dp);
+        msg=ds.toString().trim();
+        System.out.println("From Client"+msg);
+        
+        ds.close();
+        
     }
     
+}
+
+class datagram
+{
+    public static void main(String args[]) throws Exception
+    {
+        DatagramSocket ds=new DatagramSocket(2001);
+    
+        byte[] b=new byte[1024];
+        DatagramPacket dp=new DatagramPacket(b,1024);
+        ds.receive(dp);
+        String msg=dp.toString().trim();
+        System.out.println("From Client"+msg);
+        StringBuilder sb=new StringBuilder(msg);
+        sb.reverse();
+        msg=sb.toString();
+        
+        dp=new DatagramPacket(msg.getBytes(),msg.length(),InetAddress.getByName("localhost"),2000);
+        ds.send(dp);
+        ds.close();
+    
+    }
 }
