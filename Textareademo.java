ca052a65ceb22d6488300b926e59ab0559bd39e9
Rishu <rishabhkaparwan8@gmail.com>
2024-09-06 06:15:50 +0530

    6th sept,2024

--- Textareademo.java
+++ Textareademo.java
@@ -4,10 +4,54 @@
  */
 package textareademo;
 
+import java.awt.*;
+import java.awt.event.*;
+
 /**
  *
- * @author RISHU
+ * @author RISHAB
  */
+
+class myframe extends Frame
+{
+    TextArea ta;
+    Label l;
+    Button b;
+    TextField tf;
+    
+    myframe()
+    {
+        super("TextArea Demo");
+        
+        handler h=new handler();
+        
+        ta=new TextArea(10,30);
+        l=new Label("No Text Entered");
+        tf=new TextField(20);
+        
+        b=new Button("Click");
+        b.addActionListener(h);
+        
+        setLayout(new FlowLayout());
+        
+        add(ta);
+        add(l);
+        add(tf);
+        add(b);
+    }
+    
+    class handler implements ActionListener
+    {
+        public void actionPerformed(ActionEvent ae)
+        {
+//            l.setText(ta.getSelectedText //if you select the text in the text area then click button it will come on label
+//            ta.append(tf.getText());//here if you have written something in the text area and then want to append some other text in it then write in the text field and then click button
+            ta.insert(tf.getText(),ta.getCaretPosition() );//similar to append but add the text from the textfield on the position where you point your cursor line.
+            
+        }
+    }
+}
+
 public class Textareademo {
 
     /**
@@ -15,6 +59,10 @@ public class Textareademo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        myframe f=new myframe();
+        f.setSize(400, 400);
+        f.setVisible(true);
     }
     
 }
